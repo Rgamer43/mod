a7cc04e4dcf50fa1bea152f1f0e00523da3e70c9
Rgamer43 <rddellosso4@gmail.com>
2020-01-31 19:39:28 -0500

    Add Mortar_and_Pestle_T1 block Made Mortar_Pestle_Side texture Made Mortar_Pestle_Top texture

---
+++ src/main/java/net/mcreator/creation_mod/MCreatorMortarandPestleT1.java
@@ -0,0 +1,49 @@
+package net.mcreator.creation_mod;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import java.util.List;
+import java.util.Collections;
+
+@Elementscreation_mod.ModElement.Tag
+public class MCreatorMortarandPestleT1 extends Elementscreation_mod.ModElement {
+	@ObjectHolder("creation_mod:mortarandpestlet1")
+	public static final Block block = null;
+
+	public MCreatorMortarandPestleT1(Elementscreation_mod instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.DECORATIONS)).setRegistryName(block.getRegistryName()));
+	}
+
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(2f, 10f).lightValue(0).harvestLevel(1)
+					.harvestTool(ToolType.PICKAXE));
+			setRegistryName("mortarandpestlet1");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
